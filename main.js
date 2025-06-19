c210572172e32981e35b619918ff9bbd0a01477e
Harsh3822 <5100harshahire38222gmail.com>
2025-06-19 06:33:30 +0530

    ok

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 10000;
 
-// Serve static files (if frontend exists)
-app.use(express.static(path.join(__dirname, "public"))); // change if folder is 'dist' or 'build'
+// Serve static files
+app.use(express.static(path.join(__dirname, "dist")));
 
-// Route fallback (for SPAs)
+// Route fallback
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html")); // or dist/index.html
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
 });
